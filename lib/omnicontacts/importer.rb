917751bf6bbf10a647ef6556eea79decaefbfdd5
Joshua Chestang <joshua.chestang@gmail.com>
2014-04-25 10:42:15 -0500

    first commit to customize gem.

--- lib/omnicontacts/importer.rb
+++ lib/omnicontacts/importer.rb
@@ -2,6 +2,7 @@ module OmniContacts
   module Importer
 
     autoload :Gmail, "omnicontacts/importer/gmail"
+    autoload :Gmail2, "omnicontacts/importer/gmail2"
     autoload :Yahoo, "omnicontacts/importer/yahoo"
     autoload :Hotmail, "omnicontacts/importer/hotmail"
     autoload :Facebook, "omnicontacts/importer/facebook"
