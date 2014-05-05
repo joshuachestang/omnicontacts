e05410475e7a02388d9db7d58f23b4a293e6314c
Joshua Chestang <joshua.chestang@gmail.com>
2014-05-05 11:32:37 -0500

    Added additional importers for yahoo, hotmail, and Facebook for giver.

--- lib/omnicontacts/importer.rb
+++ lib/omnicontacts/importer.rb
@@ -4,8 +4,11 @@ module OmniContacts
     autoload :Gmail, "omnicontacts/importer/gmail"
     autoload :Gmail2, "omnicontacts/importer/gmail2"
     autoload :Yahoo, "omnicontacts/importer/yahoo"
+    autoload :Yahoo2, "omnicontacts/importer/yahoo2"
     autoload :Hotmail, "omnicontacts/importer/hotmail"
+    autoload :Hotmail2, "omnicontacts/importer/hotmail2"
     autoload :Facebook, "omnicontacts/importer/facebook"
+    autoload :Facebook2, "omnicontacts/importer/facebook2"
 
   end
 end
